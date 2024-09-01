ffe17c8d9e3b29679918e13bf935f45d31805062
Sathwik Amburi <sathwik.amburi@gmail.com>
2024-09-01 18:31:43 +0200

    add logger using middleware

---
+++ middleware/logger.js
@@ -0,0 +1,11 @@
+// Middleware function to log the request method and URL
+// @desc    Logs request to console
+
+const logger = (req, res, next) => {
+  console.log(
+    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
+  );
+  next();
+};
+
+module.exports = logger;
