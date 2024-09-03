1e73f62ac372718abb212f21dac1bea56a3b9f69
Sathwik Amburi <sathwik.amburi@gmail.com>
2024-09-03 16:58:42 +0200

    async handler

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,18 +1,17 @@
 const Bootcamp = require("../models/BootCamp");
+const asyncHandler = require("../middleware/async");
 const ErrorResponse = require("../utils/errorResponse");
 
 //@desc Get all bootcamps
 //@route GET /api/v1/bootcamps
 //@access Public
 
-exports.getBootcamps = async (req, res, next) => {
-  try {
-    const bootcamps = await Bootcamp.find();
-    res.status(200).json({ success: true, data: bootcamps });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }
-};
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+  const bootcamps = await Bootcamp.find();
+  res.status(200).json({ success: true, data: bootcamps });
+
+  res.status(400).json({ success: false });
+});
 
 //@desc Get all bootcamps
 //@route GET /api/v1/bootcamps/:id
