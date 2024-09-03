e1d545afa9a5db639a27b675803605dd2e8e31ae
Sathwik Amburi <sathwik.amburi@gmail.com>
2024-09-03 16:43:57 +0200

    custom error handling part 1

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,4 +1,5 @@
 const Bootcamp = require("../models/BootCamp");
+const ErrorResponse = require("../utils/errorResponse");
 
 //@desc Get all bootcamps
 //@route GET /api/v1/bootcamps
@@ -21,11 +22,16 @@ exports.getBootcamp = async (req, res, next) => {
   try {
     const bootcamp = await Bootcamp.findById(req.params.id);
     if (!bootcamp) {
-      res.status(400).json({ success: false });
+      return next(
+        new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+      );
     }
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
-    res.status(400).json({ success: false });
+    // res.status(400).json({ success: false });
+    next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
   }
 };
 
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const logger = require("./middleware/logger");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
 const colors = require("colors");
+const errorHandler = require("./middleware/error");
 
 // Route files
 
@@ -30,6 +31,7 @@ app.use(logger);
 
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
